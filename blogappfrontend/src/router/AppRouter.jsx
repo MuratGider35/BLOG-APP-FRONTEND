5be6a324205c35dc512a7c4f4421eceed6720f1f
MuratGider35 <m.gider35@gmail.com>
2023-12-03 22:56:45 +0300

    navlink

--- blogappfrontend/src/router/AppRouter.jsx
+++ blogappfrontend/src/router/AppRouter.jsx
@@ -1,10 +1,21 @@
 import React from "react";
 import Navbar from "../components/Navbar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Footer from "../components/Footer";
+import Dashboard from "../pages/Dashboard";
+import About from "../pages/About";
 
 const AppRouter = () => {
   return (
     <div>
       <Navbar />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/about" element={<About/>} />
+        </Routes>
+      </BrowserRouter>
+      <Footer />
     </div>
   );
 };
