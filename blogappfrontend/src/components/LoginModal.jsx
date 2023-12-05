5ab895488b8a5f4277d6a11e9c1419ab9b39baa2
MuratGider35 <m.gider35@gmail.com>
2023-12-05 23:28:21 +0300

    lockopenıcon

--- blogappfrontend/src/components/LoginModal.jsx
+++ blogappfrontend/src/components/LoginModal.jsx
@@ -1,29 +1,28 @@
-import * as React from 'react';
-import Backdrop from '@mui/material/Backdrop';
-import Box from '@mui/material/Box';
-import Modal from '@mui/material/Modal';
-import Fade from '@mui/material/Fade';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
+import * as React from "react";
+import Backdrop from "@mui/material/Backdrop";
+import Box from "@mui/material/Box";
+import Modal from "@mui/material/Modal";
+import Fade from "@mui/material/Fade";
+import LockOpenIcon from '@mui/icons-material/LockOpen'
+import Typography from "@mui/material/Typography";
+import { Avatar, Grid } from "@mui/material";
+
 
 const style = {
-  position: 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
   width: 400,
-  bgcolor: 'background.paper',
-  border: '2px solid #000',
+  bgcolor: "background.paper",
+  border: "2px solid #000",
   boxShadow: 24,
   p: 4,
 };
 
-export default function LoginModal({open, handleClose}) {
- 
-
+export default function LoginModal({ open, handleClose }) {
   return (
     <div>
-    
       <Modal
         aria-labelledby="transition-modal-title"
         aria-describedby="transition-modal-description"
@@ -39,6 +38,21 @@ export default function LoginModal({open, handleClose}) {
       >
         <Fade in={open}>
           <Box sx={style}>
+            <Grid container>
+              <Grid item>
+                <Avatar
+                  sx={{
+                    backgroundColor: "blue",
+                    m: "auto",
+                    mb: 3,
+                    width: 40,
+                    height: 40,
+                  }}
+                >
+                  <LockOpenIcon size="30" />
+                </Avatar>
+              </Grid>
+            </Grid>
             <Typography id="transition-modal-title" variant="h6" component="h2">
               Text in a modal
             </Typography>
