f6d45f8b688d1088fb0736cf7991be54e2275e23
MuratGider35 <m.gider35@gmail.com>
2023-12-07 00:03:17 +0300

    login modal started

--- blogappfrontend/src/components/LoginModal.jsx
+++ blogappfrontend/src/components/LoginModal.jsx
@@ -3,24 +3,42 @@ import Backdrop from "@mui/material/Backdrop";
 import Box from "@mui/material/Box";
 import Modal from "@mui/material/Modal";
 import Fade from "@mui/material/Fade";
-import LockOpenIcon from '@mui/icons-material/LockOpen'
+import LockOpenIcon from "@mui/icons-material/LockOpen";
 import Typography from "@mui/material/Typography";
-import { Avatar, Grid } from "@mui/material";
-
+import {
+  Avatar,
+  FormControl,
+  Grid,
+  IconButton,
+  Input,
+  InputAdornment,
+  InputLabel,
+  TextField,
+} from "@mui/material";
+import Visibility from "@mui/icons-material/Visibility";
+import VisibilityOff from "@mui/icons-material/VisibilityOff";
+import { useState } from "react";
 
 const style = {
   position: "absolute",
   top: "50%",
   left: "50%",
   transform: "translate(-50%, -50%)",
-  width: 400,
+  width: 500,
   bgcolor: "background.paper",
   border: "2px solid #000",
   boxShadow: 24,
-  p: 4,
+  p: 5,
 };
 
 export default function LoginModal({ open, handleClose }) {
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handleClickShowPassword = () => setShowPassword((show) => !show);
+
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  };
   return (
     <div>
       <Modal
@@ -51,14 +69,45 @@ export default function LoginModal({ open, handleClose }) {
                 >
                   <LockOpenIcon size="30" />
                 </Avatar>
+                <Typography variant="h5" color="initial" textAlign={"center"}>
+                  Join Us{" "}
+                </Typography>
+                <Box
+                  component="form"
+                  sx={{
+                    "& > :not(style)": { m: 1, width: "25ch" },
+                  }}
+                  noValidate
+                  autoComplete="off"
+                >
+                  <TextField
+                    id="standard-basic"
+                    label="Standard"
+                    variant="standard"
+                  />
+                  <FormControl sx={{ m: 1, width: "25ch" }} variant="standard">
+                    <InputLabel htmlFor="standard-adornment-password">
+                      Password
+                    </InputLabel>
+                    <Input
+                      id="standard-adornment-password"
+                      type={showPassword ? "text" : "password"}
+                      endAdornment={
+                        <InputAdornment position="end">
+                          <IconButton
+                            aria-label="toggle password visibility"
+                            onClick={handleClickShowPassword}
+                            onMouseDown={handleMouseDownPassword}
+                          >
+                            {showPassword ? <VisibilityOff /> : <Visibility />}
+                          </IconButton>
+                        </InputAdornment>
+                      }
+                    />
+                  </FormControl>
+                </Box>
               </Grid>
             </Grid>
-            <Typography id="transition-modal-title" variant="h6" component="h2">
-              Text in a modal
-            </Typography>
-            <Typography id="transition-modal-description" sx={{ mt: 2 }}>
-              Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-            </Typography>
           </Box>
         </Fade>
       </Modal>
